2d39cce9049867504908bb305684da9fe0b2f7e1
Afonso314159 <afonso.pereira1104@gmail.com>
2026-04-08 01:21:01 +0100

    added templates folder

--- com_soc/views.py
+++ com_soc/views.py
@@ -2,10 +2,13 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
-
+from django.template import loader
+from .models import Noticia
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_noticia_list = Noticia.objects.order_by("-data_criacao")[:5]
+    context = {"latest_noticia_list": latest_noticia_list}
+    return render(request, "com_soc/index.html", context)
 
 def noticia(request, noticia_id):
     return HttpResponse("You're looking at noticia %s." % noticia_id)
